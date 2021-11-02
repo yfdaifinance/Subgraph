67e5a8c77170827107b77029a7e645a65e80c7af
Kunal Dhaibar <kunal@aliteprojects.in>
2021-11-02 23:12:10 +0530

    Using old token addresses list

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -3,11 +3,11 @@ import { Pair, Token, Bundle } from '../types/schema'
 import { BigDecimal, Address, BigInt } from '@graphprotocol/graph-ts/index'
 import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD, UNTRACKED_PAIRS } from './helpers'
 
-const WETH_ADDRESS = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270'
-// const WETH_ADDRESS = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
-const USDC_WETH_PAIR = '0x7d51bad48d253dae37cc82cad07f73849286deec' // created 10008355
-const DAI_WETH_PAIR = '0xe69Fe44b087EaB9d0f1CBdcf63c1b266dcc556FE' // created block 10042267
-const USDT_WETH_PAIR = '0xb21C3387aa3dc5F476Ac8678e3F3C9520EB52eF9' // created block 10093341
+// const WETH_ADDRESS = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270'
+const WETH_ADDRESS = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270' //as it is in router
+const USDC_WETH_PAIR = '0x7d51bad48d253dae37cc82cad07f73849286deec' //invalid pair
+const DAI_WETH_PAIR = '0xe69Fe44b087EaB9d0f1CBdcf63c1b266dcc556FE' //invalid pair
+const USDT_WETH_PAIR = '0xb21C3387aa3dc5F476Ac8678e3F3C9520EB52eF9' //only valid pair
 
 export function getEthPriceInUSD(): BigDecimal {
   // fetch eth prices for each stablecoin
@@ -41,23 +41,41 @@ export function getEthPriceInUSD(): BigDecimal {
 
 // token where amounts should contribute to tracked volume and liquidity
 let WHITELIST: string[] = [
-  '0xD6DF932A45C0f255f85145f286eA0b292B21C90B', // AAVE
-  '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', // USDC
-  '0xc2132D05D31c914a87C6611C10748AEb04B58e8F', // USDT
-  '0x172370d5Cd63279eFa6d502DAB29171933a610AF', // CRV
-  '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', // DAI
-  '0x53E0bca35eC356BD5ddDFebbD1Fc0fD03FaBad39', // LINK
-  '0xb33EaAd8d922B1083446DC23f610c2567fB5180f', //UNI
-  '0x914034f0FF781c430Aa9594851cC95806fd19dc6', // SSGT
-  '0xd0cfd20E8bBDb7621B705a4FD61de2E80C2cD02F', //SSGTX
-  '0x7E7fF932FAb08A0af569f93Ce65e7b8b23698Ad8', // Yf-DAI
-  '0x3c5D1617C30BA71972adD4b0C9A6B9848f2afeeD', // DAO1
-  '0x831753DD7087CaC61aB5644b308642cc1c33Dc13', // QUICK
-  '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', // WETH
-  '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', // WMATIC
-  '0x428360b02C1269bc1c79fbC399ad31d58C1E8fdA', //DEFIT
-  '0x0EE392bA5ef1354c9bd75a98044667d307C0e773', // ORN
-  '0xF8d7195eEB28c7449f7ef8557fF9A4Da86da0c64', // I7
+  //original pairs in code
+  '0xd6df932a45c0f255f85145f286ea0b292b21c90b', // AAVE
+  '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', // USDC
+  '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', // USDT
+  '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', // WETH
+  '0x172370d5cd63279efa6d502dab29171933a610af', // CRV
+  '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063', // DAI
+  '0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39', // LINK
+  '0xb33eaad8d922b1083446dc23f610c2567fb5180f', // UNI
+  '0x914034f0ff781c430aa9594851cc95806fd19dc6', // SSGT
+  '0x7e7ff932fab08a0af569f93ce65e7b8b23698ad8', // Yf-DAI
+  '0xff835562c761205659939b64583dd381a6aa4d92', // DEXT
+  '0x0cdf14b01692c57fd8d066a053b3a0fa0aa2fc11', // FRM
+  '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270', // WMATIC
+  '0x3c5d1617c30ba71972add4b0c9a6b9848f2afeed', // DAO1
+  '0x831753dd7087cac61ab5644b308642cc1c33dc13', // QUICK
+  
+  //added tokens
+  // '0xD6DF932A45C0f255f85145f286eA0b292B21C90B', // AAVE
+  // '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', // USDC
+  // '0xc2132D05D31c914a87C6611C10748AEb04B58e8F', // USDT
+  // '0x172370d5Cd63279eFa6d502DAB29171933a610AF', // CRV
+  // '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', // DAI
+  // '0x53E0bca35eC356BD5ddDFebbD1Fc0fD03FaBad39', // LINK
+  // '0xb33EaAd8d922B1083446DC23f610c2567fB5180f', // UNI
+  // '0x914034f0FF781c430Aa9594851cC95806fd19dc6', // SSGT
+  // '0xd0cfd20E8bBDb7621B705a4FD61de2E80C2cD02F', // SSGTX
+  // '0x7E7fF932FAb08A0af569f93Ce65e7b8b23698Ad8', // Yf-DAI
+  // '0x3c5D1617C30BA71972adD4b0C9A6B9848f2afeeD', // DAO1
+  // '0x831753DD7087CaC61aB5644b308642cc1c33Dc13', // QUICK
+  // '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', // WETH
+  // '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', // WMATIC
+  // '0x428360b02C1269bc1c79fbC399ad31d58C1E8fdA', // DEFIT
+  // '0x0EE392bA5ef1354c9bd75a98044667d307C0e773', // ORN
+  // '0xF8d7195eEB28c7449f7ef8557fF9A4Da86da0c64', // I7
 
   // '0xd6df932a45c0f255f85145f286ea0b292b21c90b', // aave
   // '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', // usdc
@@ -65,15 +83,15 @@ let WHITELIST: string[] = [
   // '0x172370d5cd63279efa6d502dab29171933a610af', // crv
   // '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063', // dai
   // '0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39', // link
-  // '0xb33eaad8d922b1083446dc23f610c2567fb5180f', //uni
+  // '0xb33eaad8d922b1083446dc23f610c2567fb5180f', // uni
   // '0x914034f0ff781c430aa9594851cc95806fd19dc6', // ssgt
-  // '0xd0cfd20e8bbdb7621b705a4fd61de2e80c2cd02f', //ssgtx
+  // '0xd0cfd20e8bbdb7621b705a4fd61de2e80c2cd02f', // ssgtx
   // '0x7e7ff932fab08a0af569f93ce65e7b8b23698ad8', // yf-dai
   // '0x3c5d1617c30ba71972add4b0c9a6b9848f2afeed', // dao1
   // '0x831753dd7087cac61ab5644b308642cc1c33dc13', // quick
   // '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', // weth
   // '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270', // wmatic
-  // '0x428360b02c1269bc1c79fbc399ad31d58c1e8fda', //defit
+  // '0x428360b02c1269bc1c79fbc399ad31d58c1e8fda', // defit
   // '0x0ee392ba5ef1354c9bd75a98044667d307c0e773', // orn
   // '0xf8d7195eeb28c7449f7ef8557ff9a4da86da0c64', // i7
 ]
